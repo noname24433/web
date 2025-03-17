0ed1a020be7ed133d0bec52bfb9a229ff8d13af2
noname24433 <hardikkraj@gmail.com>
2025-03-17 13:41:38 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,63 @@
+// Define conflict ranges for vedabase URL adjustments
+const vedabaseConflicts = {
+  1: [
+    [16, 18],
+    [21, 22],
+    [32, 35],
+    [37, 38]
+  ],
+  2: [
+    [42, 43]
+  ],
+  5: [
+    [8, 9],
+    [27, 28]
+  ],
+  6: [
+    [11, 12],
+    [13, 14],
+    [20, 23]
+  ],
+  10: [
+    [4, 5],
+    [12, 13]
+  ],
+  11: [
+    [10, 11],
+    [26, 27],
+    [41, 42]
+  ],
+  12: [
+    [3, 4],
+    [6, 7],
+    [13, 14],
+    [18, 19]
+  ],
+  13: [
+    [1, 2],
+    [6, 7],
+    [8, 12]
+  ],
+  14: [
+    [22, 25]
+  ],
+  15: [
+    [3, 4]
+  ],
+  16: [
+    [1, 3],
+    [11, 12],
+    [13, 15]
+  ],
+  17: [
+    [5, 6],
+    [26, 27]
+  ],
+  18: [
+    [51, 53]
+  ]
+};
+
 class GitaApp {
   constructor() {
     this.currentVerse = null;
@@ -10,7 +70,7 @@ class GitaApp {
     this.elements = {
       newVerseBtn: document.getElementById('new-verse'),
       explainVedabase: document.getElementById('explain-vedabase'),
-      explainGitaio: document.getElementById('explain-gitaio'),
+      searchGoogle: document.getElementById('search-google'),
       searchYouTube: document.getElementById('search-youtube'),
       chapter: document.getElementById('chapter'),
       verse: document.getElementById('verse'),
@@ -23,7 +83,7 @@ class GitaApp {
   initEventListeners() {
     this.elements.newVerseBtn.addEventListener('click', () => this.loadVerse());
     this.elements.explainVedabase.addEventListener('click', () => this.openVedabase());
-    this.elements.explainGitaio.addEventListener('click', () => this.openGitaio());
+    this.elements.searchGoogle.addEventListener('click', () => this.openGoogle());
     this.elements.searchYouTube.addEventListener('click', () => this.openYouTube());
   }
 
@@ -53,22 +113,37 @@ class GitaApp {
   openVedabase() {
     if (!this.currentVerse) return;
     const { chapter, verse } = this.currentVerse;
-    window.open(`https://vedabase.io/en/library/bg/${chapter}/${verse}/`, '_blank');
+    let url = `https://vedabase.io/en/library/bg/${chapter}/${verse}/`;
+
+    // Check for conflict ranges for the current chapter
+    if (vedabaseConflicts.hasOwnProperty(chapter)) {
+      const ranges = vedabaseConflicts[chapter];
+      for (let range of ranges) {
+        const [low, high] = range;
+        if (verse >= low && verse <= high) {
+          // Build URL with the fixed range
+          url = `https://vedabase.io/en/library/bg/${chapter}/${low}-${high}/`;
+          break;
+        }
+      }
+    }
+    window.open(url, '_blank');
   }
 
-  openGitaio() {
+  openGoogle() {
     if (!this.currentVerse) return;
     const { chapter, verse } = this.currentVerse;
-    window.open(`https://bhagavadgita.io/chapter/${chapter}/verse/${verse}/`, '_blank');
+    const query = `bhagavad gita chapter ${chapter} verse ${verse}`;
+    // Use the reference URL pattern provided by the user
+    const url = `https://www.google.com/search?q=${encodeURIComponent(query)}&oq=${encodeURIComponent(query)}&gs_lcrp=EgZjaHJvbWUyDggAEEUYJxg5GIAEGIoFMggIARAAGBYYHjIICAIQABgWGB4yDQgDEAAYhgMYgAQYigUyCggEEAAYgAQYogQyCggFEAAYgAQYogQyCggGEAAYgAQYogQyCggHEAAYgAQYogTSAQgxMTUxajFqOagCCLACAfEFWoY2K7gIhBM&sourceid=chrome&ie=UTF-8`;
+    window.open(url, '_blank');
   }
 
   openYouTube() {
     if (!this.currentVerse) return;
     const { chapter, verse } = this.currentVerse;
-    // Construct the YouTube search URL using the pattern:
-    // "bhagavad gita chapter {chapter} verse {verse}"
-    const searchQuery = `bhagavad gita chapter ${chapter} verse ${verse}`;
-    const url = `https://www.youtube.com/results?search_query=${encodeURIComponent(searchQuery)}`;
+    const query = `bhagavad gita chapter ${chapter} verse ${verse}`;
+    const url = `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`;
     window.open(url, '_blank');
   }
 }
